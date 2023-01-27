94cb8c0488302b5b4a4758de6251f9165ebf49ac
mikemaniscalco <[m.maniscalco534@gmail.com]>
2023-01-26 16:15:05 -0800

    push updates

--- 2022_mdibl_data_package/EDI_package_xml_meta.R
+++ 2022_mdibl_data_package/EDI_package_xml_meta.R
@@ -1,32 +1,6 @@
-library(tidyverse) 
 library(EMLassemblyline)
-library(flextable)
-library(data.table)
-setDTthreads(threads=1)
 setwd("/Users/m.maniscalco/Dropbox/EDI_fellowship/MDIBL_phytoplankton/2022_mdibl_data_package")
 
-# template_table_attributes(path="./metadata_templates",
-#                           data.path = "./data_objects",
-#                           data.table = c("event.csv",
-#                                          "occurrence.csv",
-#                                          "ExtendedMeasurementOrFact.csv"),
-#                           write.file = TRUE,
-#                           x = NULL)
-# 
-# template_categorical_variables(path="./metadata_templates",
-#                                data.path = "./data_objects",
-#                                write.file = TRUE)
-
-# template_taxonomic_coverage(
-#   path="./metadata_templates",
-#   data.path = "./source_data",
-#   taxa.table="tax_table.csv",
-#   taxa.col="scientificName",
-#   taxa.name.type= "scientific",
-#   taxa.authority=9, 
-#   empty = FALSE,
-#   write.file = TRUE
-# )
 
 eml_doc <- make_eml(path="./metadata_templates",
                     data.path = "./data_objects",
@@ -34,24 +8,21 @@ eml_doc <- make_eml(path="./metadata_templates",
                     data.table.description= c("DwC-A Event Table","DwC-A Occurrence Table","DwC-A Extended Measurement Or Fact Table"),
                     data.table.name = c("event","occurrence","ExtendedMeasurementOrFact"),
                     eml.path = "./eml",
-                    dataset.title = "Harmful algal bloom monitoring data near Frenchmans Bay from 2004-2022",
+                    dataset.title = "Harmful algal bloom monitoring data near Frenchman Bay from 2004-2022",
                     maintenance.description = "On going collection with annual updates",
-                    temporal.coverage = c('2004-08-02', '2022-06-01'), 
+                    temporal.coverage = c('2004-08-02', '2022-07-13'), 
                     write.file = TRUE,
                     data.table.quote.character = NULL,
-                    data.table.url = NULL,
+                    # data.table.url = c("https://pasta.lternet.edu/package/data/eml/edi/8675309/1/4119639092e62c55ea8be348e4d9260d",
+                    #                    "https://pasta.lternet.edu/package/data/eml/edi/8675309/1/7ad56af78ac5f73d81f5e80293f1d6c9",
+                    #                    "https://pasta.lternet.edu/package/data/eml/edi/8675309/1/8d32e1a8d80e6db55fabf79b56a8979c"),
                     other.entity = NULL,
                     other.entity.name = NULL,
                     other.entity.description = NULL,
                     other.entity.url = NULL,
-                    user.id = "mmaniscalco",
-                    user.domain = "EDI",
-                    package.id = "edi.2691234123.1",
+                    user.id = c("mmaniscalco",'agarrets', "mohonkpreserve"),
+                    user.domain = c("EDI", 'EDI', 'EDI'),
+                    package.id = "edi.8675309.3",
                     return.obj = FALSE,
                     x = NULL)
-
-
-
-
-
 
---
+++ publish_update_to_EDI_example.R
@@ -0,0 +1,90 @@
+library(EDIutils)
+library(here)
+library(stringr)
+library(readr)
+library(tibble)
+library(lubridate)
+library(EML)
+### from: https://github.com/sccoos/HABsDataPublish/blob/master/R/publish_to_EDI.R
+### needs to be edited/adapted when updating datapackage in 2023
+## Get env vars
+if (Sys.getenv("EDI_ENV") %in% c("staging", "production")){
+  env <- Sys.getenv("EDI_ENV")
+} else {
+  throw("Invalid environment set for EDI_ENV.")
+}
+
+usern <- Sys.getenv("EDI_USER")
+passw <- Sys.getenv("EDI_PASS")
+
+## Get path to generated EML for dataset
+eml_path <- here("DwC", "datapackage")
+
+## Read in EML to update DOI
+eml_doc <- read_eml(here(eml_path, "eml.xml"))
+
+# Get most recent version of published dataset
+identifiers <- read_csv(here("EML", "package_identifiers.csv"))
+dataset_id <- identifiers[[paste0("edi_", env)]]
+
+# Get most recent version of published dataset
+current_version <- list_data_package_revisions(
+  scope = "edi",
+  identifier = dataset_id,
+  env = env,
+  filter = "newest"
+)
+new_version <- as.numeric(current_version) + 1
+
+new_doi <- paste0("edi.",dataset_id, ".", new_version)
+
+# Update EML with new DOI
+eml_doc$packageId <- new_doi
+
+# Add EDI specific access
+eml_doc$access <- list(
+  authSystem="https://pasta.lternet.edu/authentication",
+  order="allowFirst",
+  scope="document",
+  system="https://pasta.lternet.edu",
+  allow = list(
+    list(
+      principal = paste0("uid=", usern, ",o=EDI,dc=edirepository,dc=org"),
+      permission = "all"
+    ),
+    list(
+      principal = "public",
+      permission = "read"
+    )
+  )
+)
+
+# Rewrite updated EML file with EDI naming convention
+eml <- EML::write_eml(eml_doc, here(eml_path, paste0(new_doi,".xml")))
+
+## Publish to EDI using EDIutils
+tryCatch({
+  print(paste("Updating data with doi:", new_doi))
+
+  # Authenticate to EDI repository
+  EDIutils::login(userId = usern, userPass = passw)
+
+  transaction <- EDIutils::update_data_package(
+    eml = here(eml_path, paste0(new_doi,".xml")),
+    env = env
+  )
+
+  status <- EDIutils::check_status_update(transaction, wait = TRUE, env = env)
+
+  if (status) {
+    print(paste0("Updated package ", new_doi, ":"))
+    print(EDIutils::read_data_package_report_summary(new_doi, env = env))
+  }
+
+  EDIutils::logout()
+
+}, error=function(ex) {
+  print(paste("Update to EDI failed with error: ", ex))
+  EDIutils::logout()
+})
+
